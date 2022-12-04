46399069563ede26f59d1c1a8010b0b4ab07346a
kenpaso <kengalang2@gmail.com>
2022-12-03 22:25:51 -0800

    merging Ed branch (#3)
    * Creation of 2D Circle Visualizer
    
    Rough start of 2D Circle Visualizer. Circle Spikes with each note played. Builds and runs properly.
    
    * Creation of Lute Visualization
    
    Adds lute to instrument list. Shows the neck of a lute with 6 strings. Strings currently do not work.
    
    * Clickable Lute Strings
    
    Strings now dim and are clickable
    
    * Working Lute
    
    Lute plays sounds, however the sounds are similar to the piano for now.
    
    * Updated Sound Duration
    
    Sound only plays on click, since you cannot hold a note on a lute.
    
    * Visualizer Adjustments and Comments
    
    Update to visualizer size and color, and comments for readability
    
    * Lute Visuals and Clean Up
    
    Instrument looks more like a Lute. Optimized EdLute file for easy readability.
    
    * Small Adjustment on Visualizer
    
    Co-authored-by: EdmundJohnCruz <edmundjohncruz@gmail.com>

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -3,8 +3,10 @@ import { List, Map } from 'immutable';
 
 // project dependencies
 import { PianoInstrument } from './instruments/Piano';
-import { XylophoneInstrument } from './instruments/kenpaso';
 import { WaveformVisualizer } from './visualizers/Waveform';
+import { EdLute } from './instruments/EdLute';
+import { EdmundJohnCruz } from './visualizers/EdVisualizer';
+import { XylophoneInstrument } from './instruments/kenpaso';
 import { KenpasoVisualizer } from './visualizers/kenpaso';
 
 
@@ -17,13 +19,13 @@ export type AppState = Map<string, any>;           // similar to { [id: string]:
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-const instruments = List([PianoInstrument, XylophoneInstrument  ]);       // similar to Instrument[]
+const instruments = List([PianoInstrument, XylophoneInstrument, EdLute  ]);       // similar to Instrument[]
 
 /**
  * Start with the default waveform visualizer.
  * Add your visualizers to this list.
  */
-const visualizers = List([WaveformVisualizer, KenpasoVisualizer]);    // similar to Visualizer[]
+const visualizers = List([WaveformVisualizer, KenpasoVisualizer, EdmundJohnCruz]);    // similar to Visualizer[]
 
 
 /**

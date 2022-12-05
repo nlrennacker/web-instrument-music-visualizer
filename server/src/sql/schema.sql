6bcbd0e62a3618d5dd8aa539e159e1784aacc07d
kenpaso <kengalang2@gmail.com>
2022-12-04 21:26:43 -0800

    added songs.

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -9,4 +9,10 @@ INSERT INTO songs (id, song_title, notes, song_artist)
 VALUES (1, 'Ode to Joy', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4', 'Christmas Joe');
 
 INSERT INTO songs (id, song_title, notes, song_artist) 
-VALUES (2, 'Hello', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4', 'Adele');
+VALUES (2, 'Hello', 'E4 E4 E4 G4 G4 G4 E4 D4 C4 C4 D4 E4 E4 D4 D4', 'Adele');
+
+INSERT INTO songs (id, song_title, notes, song_artist) 
+VALUES (3, 'Easy On Me', 'C4 E4 D4 G4 G4 G4 E4 D4 C4 C4 E4 E4 E4 D4 D4', 'Adele');
+
+INSERT INTO songs (id, song_title, notes, song_artist) 
+VALUES (4, 'Chunky', 'C4 E4 D4 G4 G4 G4 E4 D4 C4 C4 E4 E4 E4 D4 D4', 'Bruno Mars');

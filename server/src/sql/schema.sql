55da56d1b9b81424fd624f64f4a2d17fc8ba18ee
Nathan Rennacker <nrennacker@sfsu.mail.edu>
2022-12-04 23:12:39 -0800

    comments and song

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -16,3 +16,6 @@ VALUES (3, 'Easy On Me', 'C4 E4 D4 G4 G4 G4 E4 D4 C4 C4 E4 E4 E4 D4 D4', 'Adele'
 
 INSERT INTO songs (id, song_title, notes, song_artist) 
 VALUES (4, 'Chunky', 'C4 E4 D4 G4 G4 G4 E4 D4 C4 C4 E4 E4 E4 D4 D4', 'Bruno Mars');
+
+INSERT INTO songs (id, song_title, notes, song_artist) 
+VALUES (5, 'Merry-Go-Round of Life', 'D4 G4 A#4 D5 D5 C5 A#4 A4 A#4 G4 A#4 D5 G5 G5 G5 F5 D#5 F5', 'Joe Hisaishi');
